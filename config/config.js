ad467cbaae1009359919eef3f3c7b1f66c17682c
mahmoud abdallah <mahmoud.abdallah414@yahoo.com>
2018-05-01 18:05:29 +0300

    Added files and changes required for deployment to heroku

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database:'mongodb://mean:mean@ds131782.mlab.com:31782/meanauthapp',
+    database: process.env.MONGODB_URI||'mongodb://mean:mean@ds131782.mlab.com:31782/meanauthapp',
     secret:'anyway'
 }
